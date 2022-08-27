e19b410e200199ad3f90558dc930962ae5a645a6
BDK27 <briansam195@gmail.com>
2022-08-27 16:29:38 +0800

    nav-RWD

---
+++ script.js
@@ -0,0 +1,22 @@
+let menu = document.getElementById('bar');
+let item = document.getElementById('item');
+
+item.style.right = '-300px';
+
+menu.onclick = function (){
+    if (item.style.right === '-300px'){
+        item.style.right = '0';
+    } else{
+        item.style.right = '-300px';
+    }
+}
+
+
+
+
+
+
+
+
+
+
